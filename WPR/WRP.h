707cc7eb2f83313aa11dc668052e0c2b98dc38b0
linda5150 <linda_5150@163.com>
2020-06-22 16:15:22 +0800

    add write protection setting methods

---
+++ WPR/WRP.h
@@ -0,0 +1,7 @@
+#ifndef __WRP_H_
+#define __WRP_H_
+
+#include "stm32l4xx_hal_def.h"
+uint32_t Bank1_WRP(uint32_t strtA_offset, uint32_t endA_offset);
+uint32_t Bank1_nWRP(void);
+#endif  /* __YMODEM_H_ */
