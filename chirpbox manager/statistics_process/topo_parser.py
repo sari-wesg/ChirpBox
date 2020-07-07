f490fd2467eba63c51331cdd55fafd77fbaf7405
Pei Tian <tianpei021@gmail.com>
2020-07-07 10:25:04 +0800

    update topo praser

--- chirpbox manager/statistics_process/topo_parser.py
+++ chirpbox manager/statistics_process/topo_parser.py
@@ -9,13 +9,13 @@ import matplotlib
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import seaborn as sns#style.use('seaborn-paper') #sets the size of the charts
 
-# import mplleaflet
-
 current_node_id = 0
 node_num = 0
+node_num_row = 0
+
 class STATE(enum.Enum):
-	WAITING_FOR_R = 1
-	WAITING_FOR_F = 2
+    WAITING_FOR_R = 1
+    WAITING_FOR_F = 2
 
 # style
 style.use('seaborn-talk') #sets the size of the charts
@@ -28,12 +28,12 @@ green_colors = ['#FFFFE5', '#F7FCB9', '#D9F0A3', '#ADDD8E', '#78C679', '#41AB5D'
 red_colors = ['#FFF5F0', '#FEE0D2', '#FCBBA1', '#FC9272', '#FB6A4A', '#EF3B2C', '#CB181D', '#A50F15', '#67000D']
 
 def heatmap(data,
-            row_labels,
-            col_labels,
-            ax=None,
-            cbar_kw={},
-            cbarlabel="",
-            **kwargs):
+        row_labels,
+        col_labels,
+        ax=None,
+        cbar_kw={},
+        cbarlabel="",
+        **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
@@ -61,7 +61,7 @@ def heatmap(data,
 
     # Plot the heatmap
     im = ax.imshow(data, **kwargs)
-    
+
     # Adjust the size of the color bar
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="5%", pad=0.05)
@@ -83,10 +83,10 @@ def heatmap(data,
 
     # Rotate the tick labels and set their alignment.
     plt.setp(ax.get_xticklabels(),
-             # rotation=90,
-             rotation=0,
-             ha="right",
-             rotation_mode="anchor")
+                # rotation=90,
+                rotation=0,
+                ha="right",
+                rotation_mode="anchor")
 
     # Turn spines off and create white grid.
     for edge, spine in ax.spines.items():
@@ -101,11 +101,11 @@ def heatmap(data,
 
 
 def annotate_heatmap(im,
-                     data=None,
-                     valfmt="{x:.2f}",
-                     textcolors=["black", "white"],
-                     threshold=None,
-                     **textkw):
+                    data=None,
+                    valfmt="{x:.2f}",
+                    textcolors=["black", "white"],
+                    threshold=None,
+                    **textkw):
     """
     A function to annotate a heatmap.
 
@@ -162,179 +162,205 @@ def annotate_heatmap(im,
 
 
 def topo_parser(filename, using_pos):
-	print(filename)
-	global node_num
-	state = STATE.WAITING_FOR_R
-	node_list = []
-	# 1. compute the amount of nodes
-	with open(filename, 'r') as f:
-		for line in f:
-			if(line.startswith('r ')):
-				tmp = line.split()
-				print(line)
-				current_node_id = int(tmp[1], base = 16) + int(tmp[2], base = 16) * 256
-				node_list.append(current_node_id)
-				node_num = node_num + 1
-	# 2. generate connectivity matrix
-	con_mat = np.zeros((node_num, node_num))
-	for cnt in range(node_num):
-		con_mat[cnt, cnt] = 100
+    print(filename)
+    global node_num, node_num_row
+    state = STATE.WAITING_FOR_R
+    node_list = []
+    # 1. compute the amount of nodes
+    previous_line = ''
+    str_i = ''
+    i = 0
+    k = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            if (line.startswith('f') and (previous_line.startswith('r 00'))):
+                str_i += line[2:].strip() + ' '
+                k += 1
+                if (k == 2):
+                    tmp = line.split()
+                    node_num_row = int(tmp[1], base = 16) + int(tmp[2], base = 16) * 256
+            # keep a copy of this line for the next loop
+            previous_line = line
+    # find the node_id
+    pre_c = ''
+    str_index = ''
+    for i, c in enumerate(str_i):
+        if (((i - 1) % 12) == 0) and (i != 0):
+            str_index = '0x' + pre_c + c
+            int_i = int(str_index, 16)
+            node_list.append(int_i)
+        pre_c = c
+    i = 0
+    for x in node_list:
+        i += 1
+        if ((x == 0) or (x == 255)) and (i != 1):
+            break
+    node_num = i - 1
+    print(node_list)
+    if (k == 1):
+        node_num_row = node_num
+    print(node_num, node_num_row)
 
-	# 3. update the connectivity matrix
-	with open(filename, 'r') as f:
-	  	for line in f:
-	   		if(line.startswith('r ')):
-	   			print(line, end='')
-	   			tmp = line.split()
-	   			current_node_id = int(tmp[1], base = 16) + int(tmp[2], base = 16) * 256
-	   			state = STATE.WAITING_FOR_F
-	   		if(line.startswith('f ')):
-	   			print(line, end='')
-	   			if(state == STATE.WAITING_FOR_F):
-	   				tmp = line.split()
-	   				for cnt in range(node_num):
-		   				tx_id = int(tmp[cnt * 4 + 1], base = 16) + int(tmp[cnt * 4 + 2], base = 16) * 256
-		   				reliability = (int(tmp[cnt * 4 + 3], base = 16) + int(tmp[cnt * 4 + 4], base = 16) * 256) / 100
-		   				if(int(node_list.index(current_node_id)) != int(node_list.index(tx_id))):
-		   					con_mat[node_list.index(current_node_id), node_list.index(tx_id)] = reliability
-	
-	# 4. update the connectivity matrix
-	with open(filename, 'r') as f:
-	  	for line in f:
-	   		if(line.startswith('r ')):
-	   			print(line, end='')
-	   			tmp = line.split()
-	   			current_node_id = int(tmp[1], base = 16) + int(tmp[2], base = 16) * 256
-	   			state = STATE.WAITING_FOR_F
-	   		if(line.startswith('f ')):
-	   			print(line, end='')
-	   			if(state == STATE.WAITING_FOR_F):
-	   				tmp = line.split()
-	   				for cnt in range(node_num):
-		   				tx_id = int(tmp[cnt * 4 + 1], base = 16) + int(tmp[cnt * 4 + 2], base = 16) * 256
-		   				reliability = (int(tmp[cnt * 4 + 3], base = 16) + int(tmp[cnt * 4 + 4], base = 16) * 256) / 100
-		   				if(int(node_list.index(current_node_id)) != int(node_list.index(tx_id))):
-		   					con_mat[node_list.index(current_node_id), node_list.index(tx_id)] = reliability		   					
+    # 2. generate connectivity matrix
+    con_mat = np.zeros((node_num, node_num))
+    for cnt in range(node_num):
+        con_mat[cnt, cnt] = 100
 
-	# 4. draw the connectivity matrix
-	plt.rcParams["figure.figsize"] = (20, 20)
-	fig, ax = plt.subplots()
+    # 3. update the connectivity matrix
+    with open(filename, 'r') as f:
+        for line in f:
+            if(line.startswith('rece_hash:')):
+                tmp = line.split(',')
+                current_node_id = int(tmp[0][10:])
+                state = STATE.WAITING_FOR_F
+            if(line.startswith('f ')):
+                if(state == STATE.WAITING_FOR_F):
+                    tmp = line.split()
+                    for cnt in range(node_num_row):
+                        reliability = 0
+                        tx_id = int(tmp[cnt * 4 + 1], base = 16) + int(tmp[cnt * 4 + 2], base = 16) * 256
+                        if (not (((cnt != 0) and (tx_id == 0)) or (tx_id > node_num - 1))):
+                            reliability = (int(tmp[cnt * 4 + 3], base = 16) + int(tmp[cnt * 4 + 4], base = 16) * 256) / 100
+                            if(int(node_list.index(current_node_id)) != int(node_list.index(tx_id))):
+                                con_mat[node_list.index(current_node_id), node_list.index(tx_id)] = reliability
 
-	plt.tick_params(labelsize=23)
+    # 4. update the connectivity matrix
+    with open(filename, 'r') as f:
+        for line in f:
+            if(line.startswith('rece_hash:')):
+                tmp = line.split(',')
+                current_node_id = int(tmp[0][10:])
+                state = STATE.WAITING_FOR_F
+            if(line.startswith('f ')):
+                if(state == STATE.WAITING_FOR_F):
+                    tmp = line.split()
+                    for cnt in range(node_num_row):
+                        reliability = 0
+                        tx_id = int(tmp[cnt * 4 + 1], base = 16) + int(tmp[cnt * 4 + 2], base = 16) * 256
+                        if (not (((cnt != 0) and (tx_id == 0)) or (tx_id > node_num - 1))):
+                            reliability = (int(tmp[cnt * 4 + 3], base = 16) + int(tmp[cnt * 4 + 4], base = 16) * 256) / 100
+                            if(int(node_list.index(current_node_id)) != int(node_list.index(tx_id))):
+                                con_mat[node_list.index(current_node_id), node_list.index(tx_id)] = reliability
 
-	plt.xlabel('TX Node ID', fontsize=23)
-	plt.ylabel('RX Node ID', fontsize=23)
-	im, cbar = heatmap(con_mat,
-	                   node_list,
-	                   node_list,
-	                   ax=ax,
-	                   cmap="YlGn",
-	                   cbarlabel="Receiving Packet Number")
-    
-	tmp = re.split(r'[().]', filename)
-	conf = tmp[0]
-	sequence_num = tmp[1]
+    # 4. draw the connectivity matrix
+    plt.rcParams["figure.figsize"] = (20, 20)
+    fig, ax = plt.subplots()
 
-	ax.set_title("Connectivity matrix -- " + conf + " (" + sequence_num + ")", fontsize=30)
-	fig.tight_layout()
-	im.set_clim(0, 100)
-	plt.savefig("Connectivity matrix -- " + conf + " (" + sequence_num + ").pdf", bbox_inches='tight')
-	#plt.show()
+    plt.tick_params(labelsize=23)
 
-	# 5. draw the topology
-	# 5.1 Get a adjacent matrix, i.e., remove weight information
-	plt.clf()
-	adj_mat = np.zeros((node_num, node_num))
-	for cnt_a_adj_c in range(node_num):
-		for cnt_a_adj_r in range(node_num):
-			if (con_mat[cnt_a_adj_r, cnt_a_adj_c] != 0):
-				adj_mat[cnt_a_adj_r, cnt_a_adj_c] = 1
-	print(adj_mat)
+    plt.xlabel('TX Node ID', fontsize=23)
+    plt.ylabel('RX Node ID', fontsize=23)
+    im, cbar = heatmap(con_mat,
+                        node_list,
+                        node_list,
+                        ax=ax,
+                        cmap="YlGn",
+                        cbarlabel="Receiving Packet Number")
+
+    tmp = re.split(r'[().]', filename)
+    conf = tmp[0]
+    sequence_num = tmp[1]
+
+    ax.set_title("Connectivity matrix -- " + conf + " (" + sequence_num + ")", fontsize=30)
+    fig.tight_layout()
+    im.set_clim(0, 100)
+    plt.savefig("Connectivity matrix -- " + conf + " (" + sequence_num + ").pdf", bbox_inches='tight')
+    #plt.show()
+
+    # 5. draw the topology
+    # 5.1 Get a adjacent matrix, i.e., remove weight information
+    plt.clf()
+    adj_mat = np.zeros((node_num, node_num))
+    for cnt_a_adj_c in range(node_num):
+        for cnt_a_adj_r in range(node_num):
+            if (con_mat[cnt_a_adj_r, cnt_a_adj_c] != 0):
+                adj_mat[cnt_a_adj_r, cnt_a_adj_c] = 1
+    print(adj_mat)
     # 5.2 G_DIR is a directional graph, G_UNDIR is a undirectional graph
-	G_DIR = nx.from_numpy_matrix(np.array(adj_mat), create_using = nx.MultiDiGraph())
-	G_UNDIR = nx.from_numpy_matrix(np.array(adj_mat))
-	print(G_UNDIR)
-    
-	data_dir = "."
-	posfilepath = r"\pos.npy"
-	tmp_key = []
-	for cnt in range(node_num):
-		tmp_key.append(cnt)
+    G_DIR = nx.from_numpy_matrix(np.array(adj_mat), create_using = nx.MultiDiGraph())
+    G_UNDIR = nx.from_numpy_matrix(np.array(adj_mat))
+    print(G_UNDIR)
 
-	topo_drawing_mapping = dict(zip(tmp_key, node_list))
-	G_UNDIR_MAPPING = nx.relabel_nodes(G_UNDIR, topo_drawing_mapping)
-	# if os.path.exists(data_dir + posfilepath):
-	if (using_pos == 1):
-		print(data_dir + posfilepath)
-		txt = np.load(data_dir + posfilepath, allow_pickle = True)
-		fixed_pos = txt.item()
-		fixed_nodes = fixed_pos.keys()
-		pos = nx.spring_layout(G_UNDIR_MAPPING, pos = fixed_pos, fixed = fixed_nodes, scale=3)
-	else:
-		if (using_pos == 0):
-			pos = nx.spring_layout(G_UNDIR_MAPPING)
-		if (using_pos == 2):
-			pos = {0: [373, 338], 1: [230, 319], 2: [352, 175], 3: [409, 256]}
-		# pos = {0: [373, 338], 1: [230, 319], 2: [352, 175], 3: [409, 256], 4: [249, 132], 5: [466, 184], 6: [545, 272], 7: [673, 283], 8: [217, 408], 18: [803, 267], 24: [296, 309]}
-		np.save(data_dir + posfilepath, pos)
-	if (using_pos == 2):		
-		img = matplotlib.image.imread("area1.png")
-		#plt.scatter(x,y,zorder=1)
-		plt.imshow(img, zorder = 0)
+    data_dir = "."
+    posfilepath = r"\pos.npy"
+    tmp_key = []
+    for cnt in range(node_num):
+        tmp_key.append(cnt)
 
-	nx.draw(G_UNDIR_MAPPING, pos = pos, node_color = '#ADDD8E', with_labels = True)
-	nx.draw_networkx_edges(G_UNDIR_MAPPING, pos = pos, edge_color = 'black', alpha = .1)
-	nx.draw_networkx_labels(G_UNDIR_MAPPING, pos = pos, label_pos = 10.3)
-	plt.title('Network topology -- ' + conf + ' (' + sequence_num + ')', fontsize=30)
-	plt.savefig('Network topology -- ' + conf + ' (' + sequence_num + ").pdf", bbox_inches='tight')
-	#plt.show()
-	#mplleaflet.show(fig=ax.figure)
-	#area = smopy.Map((30.0, 119.8, 30.55, 120.5),z=10)
-	
-	
- 	# 6 Get the maximal hop
-	max_hop = 0
-	no_path = 0
-	for cnt_degrees_rx in range(node_num):
-		for cnt_degrees_tx in range(node_num):
-			try:
-				hop = nx.shortest_path_length(G_DIR, source = cnt_degrees_tx, target = cnt_degrees_rx)
-			except nx.NetworkXNoPath:
-				print("No path from " + str(node_list[cnt_degrees_tx]) + " to " + str(node_list[cnt_degrees_rx]))
-				no_path = 1
-			if (max_hop < hop):
-				max_hop = hop
-				print("So far, the maximal hop is from " + str(node_list[cnt_degrees_tx]) + " to " + str(node_list[cnt_degrees_rx]))
-		if (no_path == 1):
-			max_hop = max_hop + 100
-    
+    topo_drawing_mapping = dict(zip(tmp_key, node_list))
+    G_UNDIR_MAPPING = nx.relabel_nodes(G_UNDIR, topo_drawing_mapping)
+    # if os.path.exists(data_dir + posfilepath):
+    if (using_pos == 1):
+        print(data_dir + posfilepath)
+        txt = np.load(data_dir + posfilepath, allow_pickle = True)
+        fixed_pos = txt.item()
+        fixed_nodes = fixed_pos.keys()
+        pos = nx.spring_layout(G_UNDIR_MAPPING, pos = fixed_pos, fixed = fixed_nodes, scale=3)
+    else:
+        if (using_pos == 0):
+            pos = nx.spring_layout(G_UNDIR_MAPPING)
+        if (using_pos == 2):
+            pos = {0: [373, 338], 1: [230, 319], 2: [352, 175], 3: [409, 256]}
+        # pos = {0: [373, 338], 1: [230, 319], 2: [352, 175], 3: [409, 256], 4: [249, 132], 5: [466, 184], 6: [545, 272], 7: [673, 283], 8: [217, 408], 18: [803, 267], 24: [296, 309]}
+        np.save(data_dir + posfilepath, pos)
+    if (using_pos == 2):
+        img = matplotlib.image.imread("area1.png")
+        #plt.scatter(x,y,zorder=1)
+        plt.imshow(img, zorder = 0)
+
+    nx.draw(G_UNDIR_MAPPING, pos = pos, node_color = '#ADDD8E', with_labels = True)
+    nx.draw_networkx_edges(G_UNDIR_MAPPING, pos = pos, edge_color = 'black', alpha = .1)
+    nx.draw_networkx_labels(G_UNDIR_MAPPING, pos = pos, label_pos = 10.3)
+    plt.title('Network topology -- ' + conf + ' (' + sequence_num + ')', fontsize=30)
+    plt.savefig('Network topology -- ' + conf + ' (' + sequence_num + ").pdf", bbox_inches='tight')
+    #plt.show()
+    #mplleaflet.show(fig=ax.figure)
+    #area = smopy.Map((30.0, 119.8, 30.55, 120.5),z=10)
+
+
+    # 6 Get the maximal hop
+    max_hop = 0
+    no_path = 0
+    for cnt_degrees_rx in range(node_num):
+        for cnt_degrees_tx in range(node_num):
+            try:
+                hop = nx.shortest_path_length(G_DIR, source = cnt_degrees_tx, target = cnt_degrees_rx)
+            except nx.NetworkXNoPath:
+                print("No path from " + str(node_list[cnt_degrees_tx]) + " to " + str(node_list[cnt_degrees_rx]))
+                no_path = 1
+            if (max_hop < hop):
+                max_hop = hop
+                print("So far, the maximal hop is from " + str(node_list[cnt_degrees_tx]) + " to " + str(node_list[cnt_degrees_rx]))
+        if (no_path == 1):
+            max_hop = max_hop + 100
+
     # 7 Get the Degree information of the network:
-	degrees = np.zeros(node_num)
-	for cnt_degrees_rx in range(node_num):
-		de = 0
-		for cnt_degrees_tx in range(node_num):
-			if(cnt_degrees_rx != cnt_degrees_tx):
-				if (con_mat[cnt_degrees_rx, cnt_degrees_tx] != 0):
-					de = de + 1
-		degrees[cnt_degrees_rx] = de
-	mean_degree = np.mean(degrees)
-	std_dev_degree = np.std(degrees)
-	min_degree = np.amin(degrees)
-	max_degree = np.amax(degrees)
-	print(degrees)
+    degrees = np.zeros(node_num)
+    for cnt_degrees_rx in range(node_num):
+        de = 0
+        for cnt_degrees_tx in range(node_num):
+            if(cnt_degrees_rx != cnt_degrees_tx):
+                if (con_mat[cnt_degrees_rx, cnt_degrees_tx] != 0):
+                    de = de + 1
+        degrees[cnt_degrees_rx] = de
+    mean_degree = np.mean(degrees)
+    std_dev_degree = np.std(degrees)
+    min_degree = np.amin(degrees)
+    max_degree = np.amax(degrees)
+    print(degrees)
 
-	# 8 Get the symmetry of links: (https://math.stackexchange.com/questions/2048817/metric-for-how-symmetric-a-matrix-is)
-	c = con_mat
-	c_t = np.transpose(c)
-	c_sym = 0.5 * (c + c_t)
-	c_anti = 0.5 * (c - c_t)
-	# print(c_sym)
-	# print(c_anti)
-	norm_c_sym = np.linalg.norm(c_sym,ord=2,keepdims=True)
-	norm_c_anti = np.linalg.norm(c_anti,ord=2,keepdims=True)
-	# print(norm_c_sym)
-	# print(norm_c_anti)
-	sym = (norm_c_sym - norm_c_anti) / (norm_c_sym + norm_c_anti)
-	# print(sym)
+    # 8 Get the symmetry of links: (https://math.stackexchange.com/questions/2048817/metric-for-how-symmetric-a-matrix-is)
+    c = con_mat
+    c_t = np.transpose(c)
+    c_sym = 0.5 * (c + c_t)
+    c_anti = 0.5 * (c - c_t)
+    # print(c_sym)
+    # print(c_anti)
+    norm_c_sym = np.linalg.norm(c_sym,ord=2,keepdims=True)
+    norm_c_anti = np.linalg.norm(c_anti,ord=2,keepdims=True)
+    # print(norm_c_sym)
+    # print(norm_c_anti)
+    sym = (norm_c_sym - norm_c_anti) / (norm_c_sym + norm_c_anti)
+    # print(sym)
 
-	return [max_hop, mean_degree, std_dev_degree, min_degree, max_degree, sym[0][0]]
+    return [max_hop, mean_degree, std_dev_degree, min_degree, max_degree, sym[0][0]]
