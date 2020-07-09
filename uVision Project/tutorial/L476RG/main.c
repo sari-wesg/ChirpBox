f7dff2464208a5651274ffbf98eb734742df5a15
Pei Tian <tianpei021@gmail.com>
2020-07-09 16:25:03 +0800

    14a4

--- uVision Project/tutorial/L476RG/main.c
+++ uVision Project/tutorial/L476RG/main.c
@@ -112,7 +112,7 @@ static const uint32_t nodes[256] = {0x4B0023, 0x38001E, 0x1E0030, 0x210027, 0x36
 // static const uint32_t nodes[256] = {0x4B0023, 0x360017};
 
 #endif
-const uint8_t VERSION_MAJOR = 0x95, VERSION_NODE = 0x70;
+const uint8_t VERSION_MAJOR = 0x14, VERSION_NODE = 0xa4;
 //**************************************************************************************************
 //***** Local Typedefs and Class Declarations ******************************************************
 
