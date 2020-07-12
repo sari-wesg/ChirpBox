b98a623eb8b503d97dc057cdc37cd48a16457f8d
Pei Tian <tianpei021@gmail.com>
2020-07-12 23:10:27 +0800

    update to 6 nodes (3789)

--- uVision Project/tutorial/L476RG/main.c
+++ uVision Project/tutorial/L476RG/main.c
@@ -111,7 +111,7 @@ static const uint32_t nodes[256] = {0x4B0023, 0x38001E, 0x1E0030, 0x210027, 0x36
 // static const uint32_t nodes[256] = {0x4B0023, 0x360017};
 
 #endif
-const uint8_t VERSION_MAJOR = 0xf2, VERSION_NODE = 0x75;
+const uint8_t VERSION_MAJOR = 0x37, VERSION_NODE = 0x89;
 //**************************************************************************************************
 //***** Local Typedefs and Class Declarations ******************************************************
 
