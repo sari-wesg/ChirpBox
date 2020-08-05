5491796164c50f7805661fbccf32e9193e980b9c
Pei Tian <tianpei021@gmail.com>
2020-08-05 16:33:51 +0800

    update topo test

--- chirpbox manager/chirp_test.py
+++ chirpbox manager/chirp_test.py
@@ -5,23 +5,19 @@ import time
 def generate_command_connect(slot_number, used_sf, used_payload_len):
     count = 0
     txpower = 0
-    topo_payload_len = used_payload_len
     sf = 7
-    # for sf in range(7, 13):
-        # for txpower in np.array([0, 7, 14]):
+    # for i in range(1, 50):
+    for sf in range(7, 13):
+        for txpower in np.array([0, 14]):
         # payload_len = 1
-        # for payload_len in np.array([1, 100, 200]):
-        # task_run = -connect 12 470000 -5 7 com12 20 200
-    for i in range(1, 50):
-        # time.sleep(5)
-        task_topo_run = "cbmng.py " + "-connect " + str(sf) + " " + "470000 " + str(txpower) + " " + str(used_sf) + " " + "com12 " + str(slot_number) + " " + str(topo_payload_len) + " "
-        print(task_topo_run.split())
-        cbmng.main(task_topo_run.split())
-        # time.sleep(5)
-        task_coltopo_run = "cbmng.py " + "-coltopo " + "2 " + str(used_sf) + " " + str(used_payload_len) + " " + "com12 " + str(slot_number) + " "
-        print(task_coltopo_run.split())
-        cbmng.main(task_coltopo_run.split())
-        count += 1
+            for topo_payload_len in np.array([1, 120]):
+                task_topo_run = "cbmng.py " + "-connect " + str(sf) + " " + "470000 " + str(txpower) + " " + str(used_sf) + " " + "com12 " + str(slot_number) + " " + str(topo_payload_len) + " "
+                print(task_topo_run.split())
+                # cbmng.main(task_topo_run.split())
+                task_coltopo_run = "cbmng.py " + "-coltopo " + "2 " + str(used_sf) + " " + str(used_payload_len) + " " + "com12 " + str(slot_number) + " "
+                print(task_coltopo_run.split())
+                # cbmng.main(task_coltopo_run.split())
+                count += 1
     print("count", count)
     exit(0)
 
