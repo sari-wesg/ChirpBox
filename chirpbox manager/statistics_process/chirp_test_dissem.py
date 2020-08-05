4ea96c7529d842322dc8be0e0bf36d1fd8eb4206
Pei Tian <tianpei021@gmail.com>
2020-08-06 00:36:12 +0800

    update dissem script

--- chirpbox manager/statistics_process/chirp_test_dissem.py
+++ chirpbox manager/statistics_process/chirp_test_dissem.py
@@ -11,11 +11,11 @@ import time
 # "end_address": "0807E0D0",
 def generate_command_dissem(com_serial):
     count = 0
-    payload_len = 120
+    payload_len = 200
     # for i in range(1, 50):
     # for generation_size in np.arange(1, 11, 3):
-    for generation_size in np.array([1, 4, 8]):
-        for slot_number in np.array([10, 30, 50, 70]):
+    for generation_size in np.array([4, 8]):
+        for slot_number in np.array([30, 50, 70]):
             for used_sf in np.array([7, 9, 10]):
                 task_dissem_run = "cbmng.py " + "-dissem " + '0 ' + "99c1 " + str(payload_len) + " " + str(generation_size) + " " + str(used_sf) + " " + com_serial + "3FFFFF " + str(slot_number) + " "
                 print(generation_size, slot_number, used_sf, count)
@@ -24,6 +24,7 @@ def generate_command_dissem(com_serial):
 
                 task_coldata_run = "cbmng.py " + "-coldata " + "120 " + "9 " + com_serial + "100 "
                 print(task_coldata_run)
+                time.sleep(300)
                 cbmng.main(task_coldata_run.split())
                 count += 1
 
