093714d3b0a44453e6ac1bad0ab2d6620ff1d904
Pei Tian <tianpei021@gmail.com>
2020-08-08 18:40:33 +0800

    update dissemination script

--- chirpbox manager/statistics_process/chirp_test_dissem.py
+++ chirpbox manager/statistics_process/chirp_test_dissem.py
@@ -15,9 +15,9 @@ def generate_command_dissem(com_serial):
     count = 0
     payload_len = 200
     dissem_back_sf = 7
-    dissem_back_slot = 120
+    dissem_back_slot = 60
     used_sf = 7
-    config_list = [[4, 40], [4, 50], [8, 50], [8, 70], [12, 70], [12, 90]]
+    config_list = [[32, 140], [24, 120], [48, 180], [64, 220], [32, 200]]
     for i in range(0, len(config_list)):
         generation_size = config_list[i][0]
         slot_number = config_list[i][1]
@@ -26,7 +26,7 @@ def generate_command_dissem(com_serial):
         print(task_dissem_run)
         cbmng.main(task_dissem_run.split())
 
-        task_coldata_run = "cbmng.py " + "-coldata " + "120 " + "9 " + com_serial + "100 "
+        task_coldata_run = "cbmng.py " + "-coldata " + "120 " + "7 " + com_serial + "200 "
         print(task_coldata_run)
         # time.sleep(300)
         cbmng.main(task_coldata_run.split())
@@ -36,6 +36,6 @@ def generate_command_dissem(com_serial):
     exit(0)
 
 
-com_serial = "com12 "
+com_serial = "com11 "
 generate_command_dissem(com_serial)
 
